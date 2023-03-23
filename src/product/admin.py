7bf6d0c9eb2184b1d9e55aa1b61b933d244bbeba
raafee <ammrafie@gmail.com>
2023-03-23 16:55:57 +0600

    Task1 (Data List) Done

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Variant, Product, ProductImage, \
+                    ProductVariant, ProductVariantPrice
 
 # Register your models here.
+admin.site.register([Variant, Product, ProductImage,
+                    ProductVariant, ProductVariantPrice])
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +11,23 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ListProductView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = "context"
+
+    def get_queryset(self):
+        context = []
+        for p in Product.objects.all():
+            variants = []
+            for pvp in p.productvariantprice_set.all():
+                specs = '/'.join([
+                    pvp.product_variant_one.variant_title if pvp.product_variant_one else "",
+                    pvp.product_variant_two.variant_title if pvp.product_variant_two else "",
+                    pvp.product_variant_three.variant_title if pvp.product_variant_three else "",
+                ])
+                variants.append({'specs': specs, 'price': pvp.price, 'stock': pvp.stock})
+            context.append({'data': p, 'variants': variants})
         return context
